5ca3c6a21ad7a12c0b50dcd049b2402868cadc96
Cristin021 <dayanamoya021@gmail.com>
2025-03-31 19:54:30 -0500

    Definicion y solicitus de variables

---
+++ Funciones con dos numeros/src/Funciones_con_dos_numeros.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Funciones_con_dos_numeros{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		System.out.print("Ingrese el primer numero: ");
+		int numero1 = scanner.nextInt();
+		
+		System.out.print("Ingrese el segundo numero: ");
+		int numero2 = scanner.nextInt();
+		
+		
+		scanner.close();
+	}
+}
