e20c8f43ec926a44975a482c0008cb198d279fc6
Cristin021 <dayanamoya021@gmail.com>
2025-03-31 20:17:47 -0500

    se añaden las condiciones

--- Funciones con dos numeros/src/Funciones_con_dos_numeros.java
+++ Funciones con dos numeros/src/Funciones_con_dos_numeros.java
@@ -11,6 +11,13 @@ public class Funciones_con_dos_numeros{
 		System.out.print("Ingrese el segundo numero: ");
 		int numero2 = scanner.nextInt();
 		
+		if(numero1>numero2) {
+			System.out.println("El numero mayor es: "+ numero1);
+		} else if (numero2>numero1) {
+			System.out.println("El numero mayor es: "+numero2);
+		} else {
+			System.out.println("Los numeros son iguales");
+		}
 		
 		scanner.close();
 	}
